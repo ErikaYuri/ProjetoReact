75c3b5930fb8eb0c76a9300b96c9e0bc6518f98b
ErikaYuri <erikayuri@uol.com.br>
2023-05-03 22:57:24 -0300

    adicionando componente

--- meu/src/components/HelloWorld.js
+++ meu/src/components/HelloWorld.js
@@ -1,4 +1,10 @@
-function HelloWorld() {}
+function HelloWorld() {
+    return (
+        <div>
+            <h1>Meu primeiro componente</h1>
+        </div>
+    )
+}
 
 
 export default HelloWorld

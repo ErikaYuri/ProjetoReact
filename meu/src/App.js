63a1d2881b3a2aad5bf737e5bccdbcac356c9dec
ErikaYuri <erikayuri@uol.com.br>
2023-05-04 23:18:46 -0300

    props

--- meu/src/App.js
+++ meu/src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import HelloWorld from "./components/HelloWorld";
 import SayMyName from "./components/SayMyName";
+import Pessoa from "./components/Pessoa";
 
 function App() {
+
+  const nome = "Rafael"
   return (
     <div className="App">
       <HelloWorld />
       <SayMyName nome="Erika"/>
+      <SayMyName nome={nome} />
+      <Pessoa nome="Erika" idade="43" profissao="Desenvolvedora" foto="https://via.placeholder.com/150"
     </div>
   );
 }
---
+++ meu/src/components/Pessoa.js
@@ -0,0 +1,10 @@
+function Pessoa() {
+    return <div>
+        <img src="props.foto" alt={PaymentResponse.nome} />
+        <h2>Nome: {PaymentResponse.nome}</h2>
+        <p>Idade: {PaymentResponse.idade</p>
+        <p>Profissao: {PaymentResponse.profissao}</p>
+    </div>
+}
+
+export default Pessoa

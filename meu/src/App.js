a440374f67bcdc3e2dfda30f8859ac7c1d41fec7
ErikaYuri <erikayuri@uol.com.br>
2023-05-04 22:15:40 -0300

    props

--- meu/src/App.js
+++ meu/src/App.js
@@ -2,15 +2,9 @@ import "./App.css";
 import HelloWorld from "./components/HelloWorld";
 
 function App() {
-  const name = "Erika"
-
   return (
     <div className="App">
-      <h1>Olá React!</h1>
-      <p>Meu primeiro app</p>
-      <h2>Alterando o JSX</h2>
-      <p>Olá, {name}</p>
-      <HelloWorld/>
+      <HelloWorld />
     </div>
   );
 }

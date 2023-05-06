8add3defd14b324f8537d3289023101402d386de
ErikaYuri <erikayuri@uol.com.br>
2023-05-05 23:03:31 -0300

    correcao

--- meu/src/App.js
+++ meu/src/App.js
@@ -6,13 +6,19 @@ import Pessoa from "./components/Pessoa";
 function App() {
 
   const nome = "Rafael"
+  
   return (
-    <div className="App">
+    <div> className="App"
       <HelloWorld />
       <SayMyName nome="Erika"/>
       <SayMyName nome={nome} />
-      <Pessoa nome="Erika" idade="43" profissao="Desenvolvedora" foto="https://via.placeholder.com/150"
+      <Pessoa 
+      nome="Erika" 
+      idade="43" 
+      profissao="Desenvolvedora" 
+      foto="https://via.placeholder.com/150"></Pessoa>
     </div>
+    
   );
 }
 

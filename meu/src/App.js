510130e5f1029028fde4b32aa7fc9bf7aeaefa45
ErikaYuri <erikayuri@uol.com.br>
2023-05-08 23:22:19 -0300

    fragments

--- meu/src/App.js
+++ meu/src/App.js
@@ -3,6 +3,7 @@ import HelloWorld from "./components/HelloWorld";
 import SayMyName from "./components/SayMyName";
 import Pessoa from "./components/Pessoa";
 import Frase from "./components/Frase";
+import List from "./components/List";
 
 function App() {
 
@@ -19,7 +20,9 @@ function App() {
       nome="Erika" 
       idade="43" 
       profissao="Desenvolvedora" 
-      foto="https://via.placeholder.com/150"></Pessoa>
+      foto="https://via.placeholder.com/150"
+      />
+      <List />
     </div>
     
   );
